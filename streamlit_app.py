136e5305b6649f25074496d93b8f7fc15d17d3df
Rahul Arora <rahularora241196@gmail.com>
2023-01-24 05:15:25 +0100

    Update streamlit_app.py
    New lines of Code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
+streamlit.header('Frokost Meny')
+streamlit.text('Omega 3 & Oat Meal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
